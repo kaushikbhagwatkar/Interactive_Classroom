84d4535635865d5e6049cd86bd194c5dfa7ce00d
Kaushik <kaushikbhagwatkar@gmail.com>
2014-05-31 13:19:04 -0700

    Version 1.0
    First Fully Working Prototype

---
+++ Chat/src/com/baba/chat/AudioMainActivity.java
@@ -0,0 +1,301 @@
+package com.baba.chat;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AudioMainActivity extends Activity implements OnClickListener {
+
+	Socket s, socket;
+	BufferedReader br;
+	PrintWriter pw;
+	final Context context = this;
+	String serverResponse, option, textMsg, textSubject, macAddress,path;
+	Intent confirm;
+	ImageButton raiseHandAudio;
+	ImageView dpaudio; 
+	EditText doubtText, doubtSubject;
+	TextView hiname;
+	Button sendDoubtText;
+	DataInputStream dis;
+	DataOutputStream dos;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_audiotext);
+		init(); // INITIALIZING VARIABLES
+		path=Environment.getExternalStorageDirectory().toString()+"/AakashApp/"+TestConnection.username;
+		
+		raiseHandAudio.setOnClickListener(this); // LISTENER FOR AUDIO DOUBT
+													// BUTTON
+		sendDoubtText.setOnClickListener(this); // LISTENER FOR TEXT DOUBT
+												// BUTTON
+	
+
+		Bitmap bmp = BitmapFactory.decodeFile(path+"/dp.jpg");
+
+		dpaudio.setImageBitmap(bmp);
+		
+		hiname.setText("  Hi "+TestConnection.username+" !!  ");
+	
+	
+	
+	}
+
+	public void init() {
+		raiseHandAudio = (ImageButton) findViewById(R.id.audio_doubt_button);
+		doubtText = (EditText) findViewById(R.id.text_doubt_msg);
+		sendDoubtText = (Button) findViewById(R.id.send_text_doubt);
+		doubtSubject = (EditText) findViewById(R.id.doubt_subject);
+		dpaudio=(ImageView)findViewById(R.id.dpaudio);
+		hiname=(TextView)findViewById(R.id.hi_name);
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+// Setting onclicks for options menu
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+	// TODO Auto-generated method stub
+	
+	if(item.getItemId()==R.id.action_about)
+	{
+		Toast.makeText(getApplicationContext(),"App Developed By:\n BABA Communications Pvt. Ltd. \n The Future Of Programming World",Toast.LENGTH_LONG).show();
+	}
+	
+	else if (item.getItemId()==R.id.action_settings){
+		
+		Intent iw=new Intent("com.baba.chat.SETTINGS");
+		startActivity(iw);
+		
+		
+		
+	}
+	
+	else if (item.getItemId()==R.id.action_help){
+		
+		Intent iw=new Intent("com.baba.chat.HELP");
+		startActivity(iw);
+		
+		
+		
+	}
+	
+else if (item.getItemId()==R.id.action_logout){
+		
+		Intent iw=new Intent("com.baba.chat.FIRSTMAINACTIVITY");
+		startActivity(iw);
+		
+		finish();
+		
+		
+	}
+	
+	
+	
+	return super.onOptionsItemSelected(item);
+}
+
+	
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		switch (arg0.getId()) {
+		case R.id.send_text_doubt:
+			textMsg = doubtText.getText().toString(); // READ MESSAGE
+			textSubject = doubtSubject.getText().toString(); // READ SUBJECT
+			if (textMsg.isEmpty() || textSubject.isEmpty()) { // CHECK IF ANY OF
+																// THE TWO FIELD
+																// IS EMPTY
+				Toast.makeText(AudioMainActivity.this, "All fields are mandatory",
+						Toast.LENGTH_LONG).show();
+			} else {
+				createDialog(); // CREATE A CONFIRMATION DIALOG
+			}
+			break;
+		case R.id.audio_doubt_button:
+			confirm = new Intent(AudioMainActivity.this, AudioDoubt.class); // START
+																		// AUDIO
+																		// DOUBT
+																		// SESSION
+			startActivity(confirm);
+			break;
+		}
+	}
+
+	private void createDialog() {
+		// TODO Auto-generated method stub
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setIcon(R.drawable.ic_launcher);
+		builder.setTitle("Submit this message?");
+		builder.setMessage("Subject:- " + textSubject + "\nDoubt:- \n"
+				+ textMsg);
+
+		builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.dismiss(); // DISMISS DIALOG IF NO IS CLICKED
+			}
+		});
+		builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				Log.e("yes", "yes");
+				sendTextRequest(); // SEND DOUBT IF YES IS CLICKED
+			}
+		});
+		AlertDialog alert = builder.create(); // CREATE ALERT DIALOG
+		alert.show();
+	}
+
+	private void sendTextRequest() {
+		// TODO Auto-generated method stub
+
+		Toast.makeText(getBaseContext(), "Sending...", Toast.LENGTH_LONG)
+				.show();
+		// public class ClientThread implements Runnable {
+		new Thread() {
+
+			public void run() {
+				// TODO Auto-generated method stub
+
+				try {
+
+						socket = new Socket(TestConnection.ip, TestConnection.port);
+
+					// Log.e("ClientActivity", "C: Sending command.");
+						dos=new DataOutputStream(socket.getOutputStream());
+						dis=new DataInputStream(socket.getInputStream());
+				 
+						dos.writeUTF("DOUBT");
+					
+					
+				    
+				//  	if(TestConnection.username!=null)
+						dos.writeUTF(TestConnection.username);
+						Log.d("mohit", "uname="+TestConnection.username);
+						//	if(TestConnection.roll!=null)
+						dos.writeUTF(TestConnection.roll);
+						Log.d("mohit", "roll="+TestConnection.roll);
+						
+						//	if(TestConnection.macid!=null)
+						dos.writeUTF(getMacAddress());
+						Log.d("mohit", "macid="+getMacAddress());
+							
+				    
+						dos.writeUTF(doubtSubject.getText().toString()); // SEND
+						Log.d("mohit", "doubtsub="+doubtSubject.getText().toString());
+																		// SUBJECT
+						dos.writeUTF(doubtText.getText().toString()); // SEND
+						Log.d("mohit", "doubtText="+doubtText.getText().toString());
+																// MESSAGE
+						//	pw.println(getMacAddress()); // SEND MAC ID
+						
+						
+						//Image sending
+						File filedp = new File(path+"/dp.jpg");
+						sendFile(filedp);
+
+						String msgServer = dis.readUTF(); // RECEIVE
+						Log.d("mohit", "msgServer="+msgServer);
+														// CONFIRMATION
+														// IF MESSAGE
+														// RECEIVED BY
+														// SERVER
+				    	
+						if (msgServer.contains("received")) {
+							Toast.makeText(AudioMainActivity.this, "Doubt Sent",
+								Toast.LENGTH_SHORT).show();
+						} 
+						else {
+						Toast.makeText(AudioMainActivity.this,
+								"Server Error! Doubt not sent!",
+								Toast.LENGTH_SHORT).show();
+						}
+					}
+					catch (Exception e) {
+					e.printStackTrace();
+
+					}
+					finally {
+					if (socket != null) {
+						try {
+							// close socket connection after using it
+							socket.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+
+			}// End run
+
+		}.start();		//MESSAGE SENDING THREAD STARTED
+	}
+	public void sendFile(File file) throws IOException {
+        FileInputStream fileIn = new FileInputStream(file);
+        byte[] buf = new byte[Short.MAX_VALUE];
+        int bytesRead;        
+        while( (bytesRead = fileIn.read(buf)) != -1 ) {
+            dos.writeShort(bytesRead);
+            dos.write(buf,0,bytesRead);
+        }
+        dos.writeShort(-1);
+        fileIn.close();
+    }
+	private String getMacAddress() {	//GET MAC ADDRESS ( WIFI CONNECTION NEEDED )
+		// TODO Auto-generated method stub
+		WifiManager wifiManager = (WifiManager) this
+				.getSystemService(Context.WIFI_SERVICE);
+		WifiInfo wInfo = wifiManager.getConnectionInfo();
+		return wInfo.getMacAddress();
+	}
+	
+	
+	
+	 @Override
+	    public void onBackPressed() {
+	       
+	        Toast.makeText(getApplicationContext(), "LogOut From Options To Go Back", Toast.LENGTH_SHORT).show();
+	    }
+	
+	
+
+}
+
---
+++ Chat/src/com/baba/chat/FirstMainActivity.java
@@ -0,0 +1,248 @@
+package com.baba.chat;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.view.View.OnTouchListener;
+
+
+public class FirstMainActivity extends Activity {
+	ListView list;
+	Button newaccbutton;
+	InputStream in,in1,in2,in3;
+	ScrollView parentScroll;
+	BufferedReader reader,reader1;
+	File[] users;
+	String line,line1,line2,line3;
+	 List<String> state = new ArrayList<String>();
+	 List<String> rollstate = new ArrayList<String>();
+	 List<String> passstate = new ArrayList<String>();
+	 
+	 String mypath,currentpass,currentusername,currentroll;
+	 int statecount;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		setContentView(R.layout.first_activity_main);
+		TextView noacc = (TextView)findViewById(R.id.heading);
+		
+		//
+		try{
+		mypath=Environment.getExternalStorageDirectory().toString()+"/AakashApp/";
+		
+	       int sd=0;statecount=0;
+	       users = new File(mypath).listFiles();
+	       
+	       // Storing all usernames in state and retriving password and roll nos in same order
+	       for (int i=0;i < users.length;i++) {
+	           // System.out.println(users[i]);
+	            if (users[i].isDirectory())
+		    	{
+		    		state.add(users[i].getName());
+		    		in = new FileInputStream(mypath+users[i].getName()+"/roll.txt");
+		    		in1 = new FileInputStream(mypath+users[i].getName()+"/pass.txt");
+		    	    reader = new BufferedReader(new InputStreamReader(in));
+		    	    reader1 = new BufferedReader(new InputStreamReader(in1));
+		    	    line = reader.readLine();in.close();
+		    	    line1 = reader1.readLine();in1.close();
+		    	    rollstate.add(line);
+		    	    passstate.add(line1);
+		    	    
+		    	    
+		    		statecount++;
+		    	}
+		    	
+	            
+	            
+	        }
+	
+		}
+		catch(Exception e)
+		{
+			//Toast.makeText(getApplicationContext(), e.toString(),Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(), "No Accounts Created Yet", Toast.LENGTH_LONG).show();
+			noacc.setText("NO ACCOUNTS");
+			
+		}
+		
+		
+		if (users==null|| users.length==0)
+		{
+			Toast.makeText(getApplicationContext(), "No Accounts Created Yet", Toast.LENGTH_LONG).show();
+			noacc.setText("NO ACCOUNTS");
+			
+		}
+		
+		// Converting arraylist to array
+	    
+	    final String [] web = state.toArray(new String [state.size()] );
+	    final String [] rollweb = rollstate.toArray(new String [state.size()] );
+	    final String [] passweb = passstate.toArray(new String [state.size()] );
+	    
+	    newaccbutton = (Button)findViewById(R.id.createnewacc);
+	    
+	    // Register button
+	    newaccbutton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Intent it=new Intent("com.baba.chat.LOGIN");
+				startActivity(it);
+				finish();
+				
+				
+				
+			}
+		});
+	    
+				// Setting listview
+				CustomList adapter = new CustomList(FirstMainActivity.this, web,rollweb);
+				list=(ListView)findViewById(R.id.list);
+				list.setAdapter(adapter);
+				list.setScrollingCacheEnabled(false);
+				
+				
+				// Scrolling listview
+				
+				parentScroll=(ScrollView)findViewById(R.id.scrollout);
+				
+				parentScroll.setOnTouchListener(new View.OnTouchListener() {
+
+					@Override
+					public boolean onTouch(View v, MotionEvent event) {
+						// TODO Auto-generated method stub
+						findViewById(R.id.list).getParent()
+                        .requestDisallowInterceptTouchEvent(false);
+						return false;
+					}
+	            });
+	           
+				
+				list.setOnTouchListener(new View.OnTouchListener() {
+
+	                public boolean onTouch(View v, MotionEvent event) {
+	                   // Log.v("CHILD", "CHILD TOUCH");
+	                    // Disallow the touch request for parent scroll on touch of
+	                    // child view
+	                    v.getParent().requestDisallowInterceptTouchEvent(true);
+	                    return false;
+	                }
+	            });
+				
+				
+				
+				
+				
+				
+				
+				////////////////////////
+				
+				
+				
+				
+				// Giving pop up and validation
+				
+				list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+		            @Override
+		            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+		            	
+		            	currentpass=passweb[position];
+		            	currentroll=rollweb[position];
+		            	currentusername=web[position];
+		            	
+		            	
+		            	AlertDialog.Builder alert = new AlertDialog.Builder(FirstMainActivity.this);
+
+		            	alert.setTitle("Enter Password");
+		            	alert.setMessage("Password");
+
+		            	// Set an EditText view to get user input 
+		            	final EditText input = new EditText(FirstMainActivity.this);
+		            	input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+		            	alert.setView(input);
+
+		            	alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+		            	public void onClick(DialogInterface dialog, int whichButton) {
+		            	  String value = input.getText().toString();
+		            	  
+		            	  //  Checking password
+		            	  if (value.equals(currentpass))
+		            	  {
+		            		  Toast.makeText(getApplicationContext(), "SUCCESSFUL LOGIN",Toast.LENGTH_SHORT).show();
+		            		  Intent tc=new Intent("com.baba.chat.TESTCONNECTION");
+		            		  tc.putExtra("username1", currentusername);
+		            		  tc.putExtra("roll1", currentroll);
+		            		  
+		            		  startActivity(tc);
+		            		 // finish();
+		            		  
+		            		  
+		            	  }
+		            	  
+		            	  else
+		            	  {
+		            		  Toast.makeText(getApplicationContext(), "FAILURE",Toast.LENGTH_SHORT).show();
+		            	  }
+		            	  
+		            	  }
+		            	});
+
+		            	alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+		            	  public void onClick(DialogInterface dialog, int whichButton) {
+		            	    // Canceled.
+		            	  }
+		            	});
+
+		            	alert.show();
+		            	
+		            	
+		               
+
+		                
+		                
+		            }
+		        });
+
+				
+				
+				
+	}
+		
+	// Double backpress exit
+	private static long back_pressed;
+
+	@Override
+	public void onBackPressed()
+	{
+	        if (back_pressed + 2000 > System.currentTimeMillis()) super.onBackPressed();
+	        else Toast.makeText(getBaseContext(), "Press once again to exit!", Toast.LENGTH_SHORT).show();
+	        back_pressed = System.currentTimeMillis();
+	}
+	
+}
